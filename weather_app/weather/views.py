6b79fe61f19d95a99c0f313d93275d5702934b65
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-06-11 14:50:13 +0300

    Added queries for cities

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 # Create your views here.
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=de144db93b695a9bfe6425d504b7a595'
-    city = 'Doha'
-    r = requests.get(url.format(city)).json()
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
-    }
-    print(city_weather)
+    # city = 'Ahmedabad'
+    cities = City.objects.all()
 
-    context  = {'city_weather': city_weather}
+    if request.method == 'POST':
+        pass
+    form = CityForm
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        weather_data.append(city_weather)
+    print(weather_data)
+    context  = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
