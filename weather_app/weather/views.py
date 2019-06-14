69b4bbc2654d576bde75883cf5a2fde20eb67116
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-06-14 18:55:16 +0300

    Avoid invalid city entry and duplicate cities

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -8,11 +8,23 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=de144db93b695a9bfe6425d504b7a595'
     # city = 'Ahmedabad'
     cities = City.objects.all()
-
+    err_msg = ''
     if request.method == 'POST':
-        form = CityForm(request.POST)
-        form.save()
 
+        form = CityForm(request.POST)
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = "City does not exist in the world"
+            else:
+                err_msg = 'City already exists in the database'
+
+    print(err_msg)
     form = CityForm()
     weather_data = []
 
@@ -25,6 +37,6 @@ def index(request):
             'icon': r['weather'][0]['icon']
         }
         weather_data.append(city_weather)
-    print(weather_data)
+    # print(weather_data)
     context  = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
